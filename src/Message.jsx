e9153fbf0c4a8568d692d8cb8acc8f2537e518b7
Jessica Winters <jskawinters@gmail.com>
2018-11-28 23:02:55 +0000

    Added check for notification type on messages

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,15 +1,22 @@
 import React, {Fragment} from 'react';
 
 function Message(props) {
-  return (
-    <Fragment>
-      <div className="message system">{props.message.update}</div>
-      <div className="message">
-        <span className="message-username">{props.message.username}</span>
-        <span className="message-content">{props.message.content}</span>
-      </div>
-    </Fragment>
-  );
+  if (props.message.type === 'incomingNotification') {
+    return (
+      <Fragment>
+        <div className="message system">{props.message.update}</div>
+      </Fragment>
+    );
+  } else {
+    return (
+      <Fragment>
+        <div className="message">
+          <span className="message-username">{props.message.username}</span>
+          <span className="message-content">{props.message.content}</span>
+        </div>
+      </Fragment>
+    );
+  }
 }
 
 export default Message;
